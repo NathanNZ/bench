448e80e7c464910fac9a3a1d2d9d1c79dd3b0e7a
Nathan Holland <git>
2026-05-23 01:47:28 +1200

    Use podman version, it works with docker too but for now it's easier than waiting for PR's to be approved

--- easy-install.py
+++ easy-install.py
@@ -47,13 +47,13 @@ def cprint(*args, level: int = 1):
 def clone_frappe_docker_repo() -> None:
     try:
         urllib.request.urlretrieve(
-            "https://github.com/frappe/frappe_docker/archive/refs/heads/main.zip",
+            "https://github.com/NathanNZ/frappe_podman/archive/refs/heads/main.zip",
             "frappe_docker.zip",
         )
         logging.info("Downloaded frappe_docker zip file from GitHub")
         unpack_archive("frappe_docker.zip", ".")
-        # Unzipping the frappe_docker.zip creates a folder "frappe_docker-main"
-        move("frappe_docker-main", "frappe_docker")
+        # Unzipping the frappe_docker.zip creates a folder "frappe_podman-main"
+        move("frappe_podman-main", "frappe_docker")
         logging.info("Unzipped and Renamed frappe_docker")
         os.remove("frappe_docker.zip")
         logging.info("Removed the downloaded zip file")
